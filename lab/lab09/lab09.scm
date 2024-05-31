8efb952d84177de9c95186b5282d51ee1c0310bb
chenruixin <rxchen1999@163.com>
2024-05-31 11:04:47 +0800

    531

--- lab/lab09/lab09.scm
+++ lab/lab09/lab09.scm
@@ -1,40 +1,40 @@
-(define (over-or-under num1 num2) 
-    (cond 
-      ((< num1 num2) -1)
-      ((= num1 num2) 0)
-      (else 1)
-    )
-)
-
-(define (make-adder num) 
-  (lambda (inc) (+ num inc))
-)
-
-(define (composed f g) 
-  (lambda (x) (f (g x)))
-)
-
-(define (repeat f n) 
-  (
-    if (zero? n) (lambda (x) x)
-  (composed f (repeat f (- n 1)))
-  )
-)
-
-(define (max a b)
-  (if (> a b)
-      a
-      b))
-
-(define (min a b)
-  (if (> a b)
-      b
-      a))
-
-(define (gcd a b) 
-  (cond 
-    ((zero? a) b)
-    ((zero? b) a)
-    (else (gcd (min a b) (modulo (max a b) (min a b))))
-  )
-)
+(define (over-or-under num1 num2) 
+    (cond 
+      ((< num1 num2) -1)
+      ((= num1 num2) 0)
+      (else 1)
+    )
+)
+
+(define (make-adder num) 
+  (lambda (inc) (+ num inc))
+)
+
+(define (composed f g) 
+  (lambda (x) (f (g x)))
+)
+
+(define (repeat f n) 
+  (
+    if (zero? n) (lambda (x) x)
+  (composed f (repeat f (- n 1)))
+  )
+)
+
+(define (max a b)
+  (if (> a b)
+      a
+      b))
+
+(define (min a b)
+  (if (> a b)
+      b
+      a))
+
+(define (gcd a b) 
+  (cond 
+    ((zero? a) b)
+    ((zero? b) a)
+    (else (gcd (min a b) (modulo (max a b) (min a b))))
+  )
+)
