b626673ec088cd2968b3c4ddd4db6e917ca0f0fd
chenruixin <2807504409@qq.com>
2024-06-20 16:28:10 +0800

    problem 11 over

--- projects/scheme/scheme_eval_apply.py
+++ projects/scheme/scheme_eval_apply.py
@@ -66,12 +66,14 @@ def scheme_apply(procedure, args, env):
     elif isinstance(procedure, LambdaProcedure):
         # BEGIN PROBLEM 9
         "*** YOUR CODE HERE ***"
-        child_frame = Frame(procedure.env)
-        procedure
+        lambda_frame = procedure.env.make_child_frame(procedure.formals, args)
+        return eval_all(procedure.body,lambda_frame)
         # END PROBLEM 9
     elif isinstance(procedure, MuProcedure):
         # BEGIN PROBLEM 11
         "*** YOUR CODE HERE ***"
+        muFrame = env.make_child_frame(procedure.formals, args)
+        return eval_all(procedure.body, muFrame)
         # END PROBLEM 11
     else:
         assert False, "Unexpected procedure: {}".format(procedure)
--- projects/scheme/scheme_forms.py
+++ projects/scheme/scheme_forms.py
@@ -44,6 +44,16 @@ def do_define_form(expressions, env):
         # defining a named procedure e.g. (define (f x y) (+ x y))
         # BEGIN PROBLEM 10
         "*** YOUR CODE HERE ***"
+        symbol = signature.first
+        formals = signature.rest
+        body = expressions.rest
+
+
+        lambda_expression = Pair(formals, body)
+        lambda_form = do_lambda_form(lambda_expression, env)
+
+        env.define(symbol, lambda_form)
+        return symbol
         # END PROBLEM 10
     else:
         bad_signature = signature.first if isinstance(signature, Pair) else signature
@@ -225,6 +235,7 @@ def do_mu_form(expressions, env):
     validate_formals(formals)
     # BEGIN PROBLEM 11
     "*** YOUR CODE HERE ***"
+    return MuProcedure(formals, expressions.rest)
     # END PROBLEM 11
 
 
